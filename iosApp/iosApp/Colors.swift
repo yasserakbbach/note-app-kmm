cd98c795d5f54d0106ac57d49dc911b7a8187b06
yasser.akbbach <akbbach.yasser@gmail.com>
2022-12-01 23:16:43 +0100

    Implement NoteItem and add Colors extension on iOS

---
+++ iosApp/iosApp/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  iosApp
+//
+//  Created by Yasser Akbbach on 1/12/2022.
+//  Copyright © 2022 Ressay. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    init(hex: Int64, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+}
