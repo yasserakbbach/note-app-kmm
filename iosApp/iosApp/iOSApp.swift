a25134c3a01d22302809db73a9fd8b2e72831772
yasser.akbbach <akbbach.yasser@gmail.com>
2022-12-02 00:37:17 +0100

    finish iOS side

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,15 @@
 import SwiftUI
+import shared
 
 @main
 struct iOSApp: App {
-	var body: some Scene {
-		WindowGroup {
-			ContentView()
-		}
-	}
+    private let databaseModule = DatabaseModule()
+        
+        var body: some Scene {
+            WindowGroup {
+                NavigationView {
+                    NoteListScreen(noteDataSource: databaseModule.noteDataSource)
+                }.accentColor(.black)
+            }
+        }
 }
