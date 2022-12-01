4d80830d9de8e124dddaf2beac226b3c7070cc61
yasser.akbbach <akbbach.yasser@gmail.com>
2022-12-01 22:39:52 +0100

    Implement NoteListViewModel on iOS

---
+++ iosApp/iosApp/notelist/NoteListScreen.swift
@@ -0,0 +1,21 @@
+//
+//  NoteListScreen.swift
+//  iosApp
+//
+//  Created by Yasser Akbbach on 1/12/2022.
+//  Copyright © 2022 Ressay. All rights reserved.
+//
+
+import SwiftUI
+
+struct NoteListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoteListScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        NoteListScreen()
+    }
+}
